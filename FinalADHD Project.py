310d851fd220f7c616d8a4a629d1ccbbc8092707
BradleyW1ll <134008534+BradleyW1ll@users.noreply.github.com>
2023-05-18 16:00:10 -0400

    Add files via upload

---
+++ FinalADHD Project.py
@@ -0,0 +1,38 @@
+Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:/Users/Bradley/Documents/45564.py =================
+Source		DF	Sum of Squares	Mean Square	F-statistic	p-value
+Model		1	146126.7420		146126.7420		1.1554		0.2984
+Error		16	1877368.7415		126468.4677
+Total		17	2023495.4835
+The predicted percentage of growth in ADHD diagnosis rates for annual costs of 10% of the average income in America is: 7.778781852884269
+Projected ADHD Diagnosis Rates:
+Year 2022: 5.93%
+Year 2023: 6.36%
+Year 2024: 6.78%
+Year 2025: 7.21%
+Year 2026: 7.64%
+Year 2027: 8.07%
+Year 2028: 8.49%
+Year 2029: 8.92%
+Year 2030: 9.35%
+Year 2031: 9.78%
+>>> 
+================= RESTART: C:/Users/Bradley/Documents/45564.py =================
+Source		DF	Sum of Squares	Mean Square	F-statistic	p-value
+Model		1	146126.7420		146126.7420		1.1554		0.2984
+Error		16	1877368.7415		126468.4677
+Total		17	2023495.4835
+The predicted percentage of growth in ADHD diagnosis rates for annual costs of 10% of the average income in America is: 7.778781852884269
+Projected ADHD Diagnosis Rates:
+Year 2022: 11.10%
+Year 2023: 11.90%
+Year 2024: 12.70%
+Year 2025: 13.50%
+Year 2026: 14.31%
+Year 2027: 15.11%
+Year 2028: 15.91%
+Year 2029: 16.71%
+Year 2030: 17.51%
+Year 2031: 18.31%
